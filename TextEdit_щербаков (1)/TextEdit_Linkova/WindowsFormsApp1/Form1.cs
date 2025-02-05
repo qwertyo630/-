7bc994e62c376d1fa77b50d565663913017c7644
qwertyo630 <sabaka.0806m@gmail.com>
2025-02-05 17:47:58 +0300

    Add files via upload

---
+++ TextEdit_щербаков (1)/TextEdit_Linkova/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttoncut_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void buttoncope_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void buttonpaste_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void buttondel_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void buttonclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttoncolor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                richTextBox1.ForeColor = colorDialog1.Color;
+        }
+
+        private void buttonfont_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+                richTextBox1.Font = fontDialog1.Font;
+        }
+
+        private void buttonopen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                richTextBox1.Clear();
+            richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+        }
+
+        private void buttonsave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+        }
+
+        private void выравниваниеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void поПравомуКраюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void поЦентруToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void поЛевомуКраюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void оПрограммеToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox1 = new AboutBox1();
+            aboutBox1.Show();
+            this.Hide();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font myFont = new Font("Tahoma", 12, FontStyle.Regular, GraphicsUnit.Pixel);
+            string Hello = "Hello World!";
+            e.Graphics.DrawString(Hello, myFont, Brushes.Black, 20, 20);
+        }
+
+        private void pageSetupDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (printDialog1.ShowDialog() == DialogResult.OK) printDocument1.Print();
+        }
+
+        private void предварительныйПросмотрToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void параметрыСтраницыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pageSetupDialog1.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
